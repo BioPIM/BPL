ffdc89e3dc5b3fef8702fa2cda954d1218ddbf53
edrezen <edrezen>
2026-01-13 17:20:07 +0100

    doxygen + code cleaning

--- src/bpl/arch/ArchUpmemMetadata.hpp
+++ src/bpl/arch/ArchUpmemMetadata.hpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // BPL, the Process In Memory library for bioinformatics 
-// date  : 2024
+// date  : 2026
 // author: edrezen
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -22,15 +22,23 @@ namespace bpl  {
 // will be sent to the host and will be available through the calling launcher.
 ////////////////////////////////////////////////////////////////////////////////
 
+/** \brief Generic class that gathers time information during a tasklet execution.
+ */
 template<typename T>
 struct TimeStatsValues
 {
+    /** Total time of the tasklet. */
     T all         = T();
+    /** Time taken by a tasklet for the unserialization of the incoming arguments from the host. */
     T unserialize = T();
+    /** Time taken by a tasklet for splitting the arguments encapsulated by a call to 'split' at host call site. */
     T split       = T();
+    /** Time taken by a tasklet for the running the task, ie. call to operator(). */
     T exec        = T();
+    /** Time taken by a tasklet for preparing the result for the host. */
     T result      = T();
 
+    /** Increase the fields values of the current object with the fields values of an input object. */
     template<typename T1>
     TimeStatsValues<T>& operator+= (const TimeStatsValues<T1>& o)
     {
@@ -43,6 +51,11 @@ struct TimeStatsValues
     }
 };
 
+/** \brief Class that gathers time information during a tasklet execution.
+ *
+ * Uses a specific type for gathering the information.
+ * \see TimeStatsValues
+ */
 struct TimeStats : TimeStatsValues<uint64_t>
 {
     using value_type = uint64_t;
@@ -94,19 +107,28 @@ struct TimeStats : TimeStatsValues<uint64_t>
     }
 };
 
+/** \brief provides information about memory allocations (notably for the MRAM)
+ */
 struct AllocatorStats
 {
+    /** Bytes number used in MRAM. */
     uint32_t used        = 0;
+    /** Position in MRAM of the last pointer. */
     uint32_t pos         = 0;
+    /** Number of calls to the bpl::MRAM::get function. */
     uint32_t nbCallsGet  = 0;
+    /** Number of calls to the bpl::MRAM::read function. */
     uint32_t nbCallsRead = 0;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // INPUT
 ////////////////////////////////////////////////////////////////////////////////
+/** \brief Structure holding metadata sent from the host to the DPU.
+ */
 struct MetadataInput
 {
+    /** Maximum number of parameters allowed in the prototype of a task (should be better defined at another location). */
     static constexpr int ARGS_MAX_NUMBER = 32;
 
     MetadataInput ()  {  for (size_t i=0; i<ARGS_MAX_NUMBER; i++)  {  argsSplitStatus[i] = 0; }  }
@@ -117,38 +139,64 @@ struct MetadataInput
         for (size_t i=0; i<ARGS_MAX_NUMBER; i++)  {  argsSplitStatus[i] = splitStatus[i]; }
     }
 
+    /** Not used. */
     uint32_t nbtaskunits = 0;
+    /** Holds a logical DPU identifier. Allows to compute a global tasklet uid. */
     uint32_t dpuid       = 0;
+    /** Provides information where the beginning of the MRAM should be available to the user. */
     uint32_t bufferSize  = 0;
+    /** Not used. */
     uint32_t deltaOnce   = 0;
+    /** Allows to reset the MRAM allocator. */
     uint32_t reset       = 0;
+    /** Padding information when the tag 'once' has been used on a parameter of the task. */
     uint32_t oncePadding = 0;
+    /** Gives the split status for the arguments of the task. */
     uint8_t  argsSplitStatus[ARGS_MAX_NUMBER];
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // OUTPUT
 ////////////////////////////////////////////////////////////////////////////////
+/** \brief Structure holding metadata sent from the DPU to the host.
+ */
 struct MetadataOutput
 {
+    /** Keeps information for a vector (address and nb items) when it is contiguous in MRAM.
+     * With this information, the host can proceed some optimization for the serialization in
+     * order to retrieve the vector data.
+     */
     struct VectorInfo {
         uint32_t address = 0;
         uint32_t nbitems = 0;
     };
 
+    /** Provides the global tasklet ids, ie something like dpuid*NR_TASKLETS + me(). */
     uint32_t        result_tasklet_order [NR_TASKLETS];
+    /** Data size to be broadcasted from DPU to host. */
     uint32_t        result_tasklet_size  [NR_TASKLETS];
+    /** Time statistics at different points of a tasklet. */
     TimeStats       nb_cycles            [NR_TASKLETS];
+    /** Provides MRAM address information when the task result is a vector. */
     VectorInfo      vector_info          [NR_TASKLETS];
+    /** Statistics from the MRAM allocator. */
     AllocatorStats  allocator_stats;
+    /** MRAM pointer after task execution. */
     uint32_t        heap_pointer      = 0;
+    /** MRAM pointer before task execution. */
     uint32_t        heap_pointer_init = 0;
+    /** Number of clocks per second (provided by UPMEM) */
     uint32_t        clocks_per_sec    = 0;
+    /** Stack size (corresponds to the constant STACK_SIZE_DEFAULT) */
     uint32_t        stack_size        = 0;
+    /** Not used */
     uint32_t        input_sizeof      = 0;
+    /** sizeof of the result of the task. */
     uint32_t        output_sizeof     = 0;
+    /** Not used */
     uint32_t        restoreNbErrors   = 0;
 
+    /** \brief Provides some statistics for the bpl::vector class (for debug purpose). */
     struct VectorStats
     {
         uint32_t SIZEOF;
--- src/bpl/utils/TimeUtils.hpp
+++ src/bpl/utils/TimeUtils.hpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // BPL, the Process In Memory library for bioinformatics 
-// date  : 2024
+// date  : 2026
 // author: edrezen
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -14,26 +14,42 @@
 namespace bpl  {
 ////////////////////////////////////////////////////////////////////////////////
 
+/** Returns a timestamp (in microseconds) of the current time.
+ */
 static inline auto  timestamp()
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/** \brief Utility for computing duration between two execution points.
+ *
+ * The variable that holds the duration is given as a reference to the constructor; a timestamp t0 is then generated.
+ *
+ * When the destructor is invoked, a timestamp t1 is generated and the referred variable is set to be t1-t0.
+ *
+ * It remains possible to explicitely call 'start' and 'stop'.
+ */
 class TimeStamp
 {
 public:
 
+    /** Constructor.
+     * \param ref: a reference on the variable that will hold the duration.
+     */
     TimeStamp(float& ref) : ref_(ref)  {  start();  }
 
+    /** Destructor.  The final timestamp is generated and the duration computed. */
     ~TimeStamp()  { stop();  }
 
+    /** Generates the initial timestamp. */
     void start ()
     {
         started_ = true;
         t0_ = t1_ = timestamp();
     }
 
+    /** Generates the final timestamp. */
     void stop  ()
     {
         if (started_)
@@ -52,6 +68,10 @@ private:
 };
 
 ////////////////////////////////////////////////////////////////////////////////
+
+/** \brief Manages two TimeStamp objects at the same time.
+ * \see TimeStamp.
+ */
 class DualTimeStamp
 {
 public:
